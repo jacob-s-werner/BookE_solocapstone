20316ffa1a11de13e0273dd2e998d66987f6c216
Jacob Werner <jacob.scott.werner@gmail.com>
2021-04-27 12:23:52 -0500

    Creates Artist Model.

---
+++ BookEWebsite/Models/Artist.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookEWebsite.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Specialization { get; set; }
+        public string Description { get; set; }
+        public bool LookingForGigs { get; set; }
+        public string SizeOfGroup { get; set; }
+        public double? HourlyCost { get; set; }
+        public double? WeekendHourlyCost { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
