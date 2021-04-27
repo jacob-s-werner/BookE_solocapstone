65720029ebb5e3b955b554733706d27f6fda3eea
Jacob Werner <jacob.scott.werner@gmail.com>
2021-04-27 14:31:39 -0500

    Creates ArtistsController with skeleton Views.

---
+++ BookEWebsite/Controllers/ArtistsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookEWebsite.Data;
+using BookEWebsite.Models;
+
+namespace BookEWebsite.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Artists
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Artists.Include(a => a.Address).Include(a => a.IdentityUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Artists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .Include(a => a.Address)
+                .Include(a => a.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // GET: Artists/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id");
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Artists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PhoneNumber,EmailAddress,Specialization,Description,LookingForGigs,GroupName,SizeOfGroup,HourlyCost,WeekendHourlyCost,IdentityUserId,AddressId")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(artist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id", artist.AddressId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", artist.IdentityUserId);
+            return View(artist);
+        }
+
+        // GET: Artists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists.FindAsync(id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id", artist.AddressId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", artist.IdentityUserId);
+            return View(artist);
+        }
+
+        // POST: Artists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,PhoneNumber,EmailAddress,Specialization,Description,LookingForGigs,GroupName,SizeOfGroup,HourlyCost,WeekendHourlyCost,IdentityUserId,AddressId")] Artist artist)
+        {
+            if (id != artist.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(artist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtistExists(artist.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id", artist.AddressId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", artist.IdentityUserId);
+            return View(artist);
+        }
+
+        // GET: Artists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .Include(a => a.Address)
+                .Include(a => a.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // POST: Artists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artist = await _context.Artists.FindAsync(id);
+            _context.Artists.Remove(artist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtistExists(int id)
+        {
+            return _context.Artists.Any(e => e.Id == id);
+        }
+    }
+}
