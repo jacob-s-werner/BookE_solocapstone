6e2e0825a571a5c4287b0691281bd3034a6868c2
Jacob Werner <jacob.scott.werner@gmail.com>
2021-04-27 14:33:53 -0500

    Creates BusinessesController with CRUD skeleton Views.

---
+++ BookEWebsite/Views/BusinessesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookEWebsite.Data;
+using BookEWebsite.Models;
+
+namespace BookEWebsite.Views
+{
+    public class BusinessesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BusinessesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Businesses
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Businesses.Include(b => b.Address).Include(b => b.IdentityUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Businesses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var business = await _context.Businesses
+                .Include(b => b.Address)
+                .Include(b => b.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            return View(business);
+        }
+
+        // GET: Businesses/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id");
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Businesses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,PhoneNumber,EmailAddress,Specialization,Description,SeatingCapacity,Stage,StageCapacity,HourlyCost,WeekendHourlyCost,IdentityUserId,AddressId")] Business business)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(business);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id", business.AddressId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", business.IdentityUserId);
+            return View(business);
+        }
+
+        // GET: Businesses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var business = await _context.Businesses.FindAsync(id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id", business.AddressId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", business.IdentityUserId);
+            return View(business);
+        }
+
+        // POST: Businesses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,PhoneNumber,EmailAddress,Specialization,Description,SeatingCapacity,Stage,StageCapacity,HourlyCost,WeekendHourlyCost,IdentityUserId,AddressId")] Business business)
+        {
+            if (id != business.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(business);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BusinessExists(business.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "Id", business.AddressId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", business.IdentityUserId);
+            return View(business);
+        }
+
+        // GET: Businesses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var business = await _context.Businesses
+                .Include(b => b.Address)
+                .Include(b => b.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            return View(business);
+        }
+
+        // POST: Businesses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var business = await _context.Businesses.FindAsync(id);
+            _context.Businesses.Remove(business);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BusinessExists(int id)
+        {
+            return _context.Businesses.Any(e => e.Id == id);
+        }
+    }
+}
