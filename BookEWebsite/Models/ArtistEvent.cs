2ea6f74a5c6068b06dab8017f99ea1f4451e9da7
Jacob Werner <jacob.scott.werner@gmail.com>
2021-04-27 13:10:52 -0500

    Creates ArtistEvent Method and properties.

---
+++ BookEWebsite/Models/ArtistEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookEWebsite.Models
+{
+    public class ArtistEvent
+    {
+        [Key]
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime StartTime { get; set; } // Will convert // https://docs.microsoft.com/en-us/dotnet/api/system.datetime.tostring?redirectedfrom=MSDN&view=net-5.0#System_DateTime_ToString_System_String_
+        public DateTime EndTime { get; set; }
+        public DateTime TimeBooked { get; set; }
+        public double Cost { get; set; }
+        public double? Tip { get; set; }
+        
+        [ForeignKey("Artist")]
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+
+        [ForeignKey("Business")]
+        public int BusinessId { get; set; }
+        public Business Business { get; set; }
+    }
+}
