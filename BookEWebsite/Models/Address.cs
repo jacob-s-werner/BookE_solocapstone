141f2213c18e70260acabbdd9b2e0dec41ac37ec
Jacob Werner <jacob.scott.werner@gmail.com>
2021-04-27 12:35:24 -0500

    Creates Address Model and properties.

---
+++ BookEWebsite/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookEWebsite.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street1 { get; set;}
+        public string Street2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+    }
+}
