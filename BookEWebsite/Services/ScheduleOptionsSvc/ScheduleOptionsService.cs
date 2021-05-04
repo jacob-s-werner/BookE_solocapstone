c529c9a803f4f35e05901e22c598d8678e596d39
Jacob Werner <jacob.scott.werner@gmail.com>
2021-05-04 13:13:50 -0500

    Creates CompareAvailabilityToBookTimes method in ScheduleOptionsService; takes in availability times and checks against book times returning true if it is possible to book time. Creates CompareEventTimesToBookTimes method; takes in event times and book times, checks if event times are outside of book times or start when end/end when start. Comments out unused Methods for later feature use.

--- BookEWebsite/Services/ScheduleOptionsSvc/ScheduleOptionsService.cs
+++ BookEWebsite/Services/ScheduleOptionsSvc/ScheduleOptionsService.cs
@@ -1,5 +1,7 @@
-﻿using BookEWebsite.Models;
+﻿using BookEWebsite.Data;
+using BookEWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,18 +18,92 @@ namespace BookEWebsite.Services.ScheduleOptionsSvc
 
         public ScheduleOptionsService()
         {
-            Hours = new string[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+            Hours = new string[] { "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
             Minutes = new string[] { "00", "15", "30", "45" };
             TimeOfDay = new string[] { "AM", "PM" };
             DaysOfTheWeek = new string[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
         }
 
-        public bool CheckIfTimeSlotsAvailable(List<IAvailability> availToCheck, List<BusinessEvent> bEvents, List<ArtistEvent> aEvents)
+        //public void CheckTimeSlotsAvailable(List<ArtistAvailability> availToCheck, List<BusinessEvent> bEvents, List<ArtistEvent> aEvents, DateTime dayToCheck)
+        //{
+        //    foreach (var avail in availToCheck)
+        //    {
+        //        avail.StartTime;
+        //        avail.EndTime;
+
+        //        foreach (var b in bEvents)
+        //        {
+
+        //        }
+        //        foreach (var a in aEvents)
+        //        {
+
+        //        }
+
+        //    }
+
+        //    dayToCheck.
+
+
+        //    if (true){ return true; }
+        //    return false;
+        //}
+
+        public bool CompareAvailabilityToBookTimes(DateTime availStart, DateTime availEnd, DateTime bookStart, DateTime bookEnd)
         {
-
-
-            if (true){ return true; }
+            if ((availStart < bookStart || availStart == bookStart) && (availEnd > bookEnd || availEnd == bookEnd))
+            {
+                return true;
+            }
             return false;
         }
+
+        public bool CompareEventTimesToBookTimes(DateTime eventStart, DateTime eventEnd, DateTime bookStart, DateTime bookEnd)
+        {
+            if (((eventStart < bookStart || eventStart == bookStart) && (eventEnd < bookStart || eventEnd == bookStart)) ||
+               ((eventStart > bookEnd || eventStart == bookEnd) && (eventEnd > bookEnd || eventEnd == bookEnd)))
+            {
+                return true;
+            }
+            else if ((eventStart > bookStart && (eventEnd > bookStart || eventEnd == bookStart)) && 
+                ((eventStart > bookEnd || eventStart == bookEnd) && eventEnd > bookEnd))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //public async Task<string> MakeStringOfAvailabilities(int id, bool isArtist, ApplicationDbContext _appDBContext, DateTime dayToCheck)
+        //{
+        //    List<BusinessEvent> userBEvents;
+        //    List<ArtistEvent> userAEvents;
+        //    List<IAvailability> userAvailList = new List<IAvailability>();
+
+        //    if (isArtist)
+        //    {
+        //        var availList = await _appDBContext.ArtistAvailabilities.Where(a => a.ArtistId.Equals(id)).ToListAsync();
+        //        userBEvents = await _appDBContext.BusinessEvents.Where(a => a.ArtistId.Equals(id)).ToListAsync();
+        //        userAEvents = await _appDBContext.ArtistEvents.Where(a => a.ArtistId.Equals(id)).ToListAsync();
+        //        foreach (var avail in availList)
+        //        {
+        //            userAvailList.Add(avail);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        var availList = await _appDBContext.BusinessAvailabilities.Where(a => a.BusinessId.Equals(id)).ToListAsync();
+        //        userBEvents = await _appDBContext.BusinessEvents.Where(a => a.BusinessId.Equals(id)).ToListAsync();
+        //        userAEvents = await _appDBContext.ArtistEvents.Where(a => a.BusinessId.Equals(id)).ToListAsync();
+        //        foreach (var avail in availList)
+        //        {
+        //            userAvailList.Add(avail);
+        //        }
+        //    }
+
+        //    CheckTimeSlotsAvailable(userAvailList, userBEvents, userAEvents, dayToCheck);
+
+        //    return availHTMLString;
+        //}
+
     }
 }
