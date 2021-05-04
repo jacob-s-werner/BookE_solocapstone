121d11505cb6e92bdd4b395d19752580653f6d7f
Jacob Werner <jacob.scott.werner@gmail.com>
2021-05-03 20:19:34 -0500

    Creates a ScheduleOptionsService class in Service folder; constructs and assigns values to properties (hours,minutes,timeofday,daysoftheweek) for use in scheduling.

---
+++ BookEWebsite/Services/ScheduleOptionsSvc/ScheduleOptionsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookEWebsite.Services.ScheduleOptionsSvc
+{
+    public class ScheduleOptionsService
+    {
+        public string[] Hours { get;}
+        public string[] Minutes { get; }
+
+        public string[] TimeOfDay { get; }
+        public string[] DaysOfTheWeek { get; }
+
+        public ScheduleOptionsService()
+        {
+            Hours = new string[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+            Minutes = new string[] { "00", "15", "30", "45" };
+            TimeOfDay = new string[] { "AM", "PM" };
+            DaysOfTheWeek = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+        }
+    }
+}
